157a7dd4fe718f48868d7b0abe5e9b894c548739
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-09-26 17:42:14 -0400

    Simplified test code

--- RaspPiPythonScripts/TestNeoPixelMatrix.py
+++ RaspPiPythonScripts/TestNeoPixelMatrix.py
@@ -4,11 +4,9 @@ from NeoPixelMatrix import NeoPixelMatrix
 from CircuitPlayGround import CircuitPlayGround
 
 lastDrawTime = 0
+
 circuitPlayground = CircuitPlayGround('/dev/cu.usbmodem1411')
 neoMatrix = NeoPixelMatrix('/dev/cu.usbmodem2115241')
-neoMatrix.SetAllRed(255)
-
-circuitPlayground.Read()
 
 while 1:
 	if time.time() - lastDrawTime > 0.03 :

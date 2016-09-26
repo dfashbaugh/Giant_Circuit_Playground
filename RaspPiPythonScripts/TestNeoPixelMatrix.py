82997ff763ad00e5fd029812538314c58f1ff18b
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-09-26 17:20:12 -0400

    Readded the useful code deleted in the last commit

---
+++ RaspPiPythonScripts/TestNeoPixelMatrix.py
@@ -0,0 +1,19 @@
+import time
+import serial
+from NeoPixelMatrix import NeoPixelMatrix
+from CircuitPlayGround import CircuitPlayGround
+
+lastDrawTime = 0
+circuitPlayground = CircuitPlayGround('/dev/cu.usbmodem1411')
+neoMatrix = NeoPixelMatrix('/dev/cu.usbmodem2115241')
+neoMatrix.SetAllRed(255)
+
+circuitPlayground.Read()
+
+while 1:
+	if time.time() - lastDrawTime > 0.03 :
+		neoMatrix.DrawLEDMemory()
+		lastDrawTime = time.time()
+
+	circuitPlayground.Read()
+	neoMatrix.SetAllRed(circuitPlayground.Light)

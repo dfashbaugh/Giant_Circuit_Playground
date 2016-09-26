7626787fa305aed2d0059d4d98744e7fd02e8237
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-09-26 17:17:55 -0400

    Improved responsiveness of NeoPixel Matrix

--- ReadUARTDrawFastPWM/TestNeoPixelMatrix.py
+++ ReadUARTDrawFastPWM/TestNeoPixelMatrix.py
@@ -3,6 +3,7 @@ import serial
 from NeoPixelMatrix import NeoPixelMatrix
 from CircuitPlayGround import CircuitPlayGround
 
+lastDrawTime = 0
 circuitPlayground = CircuitPlayGround('/dev/cu.usbmodem1411')
 neoMatrix = NeoPixelMatrix('/dev/cu.usbmodem2115241')
 neoMatrix.SetAllRed(255)
@@ -10,7 +11,9 @@ neoMatrix.SetAllRed(255)
 circuitPlayground.Read()
 
 while 1:
-	neoMatrix.DrawLEDMemory()
+	if time.time() - lastDrawTime > 0.03 :
+		neoMatrix.DrawLEDMemory()
+		lastDrawTime = time.time()
+
 	circuitPlayground.Read()
-	#print circuitPlayground.Light
 	neoMatrix.SetAllRed(circuitPlayground.Light)

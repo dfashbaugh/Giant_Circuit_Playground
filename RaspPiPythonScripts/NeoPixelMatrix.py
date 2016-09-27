6ee93c13cef63353ebad6f523e86f75ac480f198
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-09-27 13:57:28 -0400

    Can now draw images to the NeoPixel

--- RaspPiPythonScripts/NeoPixelMatrix.py
+++ RaspPiPythonScripts/NeoPixelMatrix.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from PIL import Image
 
 class NeoPixelMatrix:
 
@@ -27,6 +28,31 @@ class NeoPixelMatrix:
 				self.LEDMemory[x] = 0x00
 	
 	def SetImage(self, image, x, y):
+		px = image.load()
+		#Bin 16 -> 1
+		neoImage = Image.new("RGB", (8,8))
+		neoPix = neoImage.load()
+		for x in range(0, 8) :
+			for y in range(0, 8) :
+				channel1 = 0
+				channel2 = 0
+				channel3 = 0
+				for subX in range(0,4) :
+					for subY in range(0, 4) :
+						theColor = px[4*x+subX, 4*y+subY]
+						channel1 = channel1 + theColor[0]
+						channel2 = channel2 + theColor[1]
+						channel3 = channel3 + theColor[2]
+				channel1 = channel1/16
+				channel2 = channel2/16
+				channel3 = channel3/16
+				neoPix[x,y] = (channel1, channel2, channel3)
+
+		for memoryPos in range(0, 64) :
+			self.LEDMemory[memoryPos*3] = neoPix[memoryPos%8, memoryPos/8][0]
+			self.LEDMemory[memoryPos*3+1] = neoPix[memoryPos%8, memoryPos/8][1]
+			self.LEDMemory[memoryPos*3+2] = neoPix[memoryPos%8, memoryPos/8][2]
+
 		print "Set an image"
 
 	def DrawLEDMemory(self) :
--- RaspPiPythonScripts/TestNeoPixelMatrix.py
+++ RaspPiPythonScripts/TestNeoPixelMatrix.py
@@ -2,11 +2,22 @@ import time
 import serial
 from NeoPixelMatrix import NeoPixelMatrix
 from CircuitPlayGround import CircuitPlayGround
+from PIL import Image
+from PIL import ImageDraw
+
+image = Image.new("RGB", (32, 32))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, 31, 31), fill=(0, 100, 0), outline=(0,0,255))
+draw.line( (0,0,31,31), fill=(255,0,0) )
+draw.line( (0, 31, 31, 0), fill=(255,0,0) )
 
 lastDrawTime = 0
 
 circuitPlayground = CircuitPlayGround('/dev/cu.usbmodem1411')
 neoMatrix = NeoPixelMatrix('/dev/cu.usbmodem2115241')
+
+neoMatrix.Clear()
+neoMatrix.SetImage(image, 0, 0)
 
 while 1:
 	if time.time() - lastDrawTime > 0.03 :
@@ -14,4 +25,4 @@ while 1:
 		lastDrawTime = time.time()
 
 	circuitPlayground.Read()
-	neoMatrix.SetAllRed(circuitPlayground.Cap12/10)
+	#neoMatrix.SetAllRed(circuitPlayground.Cap12/10)
